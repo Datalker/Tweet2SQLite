242e3612eea8747a72dd2d2022a32afb05c0e350
Evgeny <e.arnautov@gmail.com>
2016-03-19 19:43:48 +0000

    first

---
+++ declaration.py
@@ -0,0 +1,42 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+
+class Person(Base):
+    __tablename__ = 'person'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(250), nullable=False)
+    screen_name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+    location = Column(String(250), nullable=False)
+    url = Column(String(250))
+
+    followers_count = Column(Integer, default=0)
+    statuses_count = Column(Integer, default=0)
+    friends_count = Column(Integer, default=0)
+
+    def GetFields():
+        f = ('id', 'screen_name', 'name', 'description', 'location',
+            'url', 'followers_count', 'statuses_count', 'friends_count')
+        return f
+
+
+class Status(Base):
+    __tablename__ = 'status'
+    id = Column(String(250), primary_key=True, unique=True)
+    text = Column(String(250), nullable=False)
+    created_at = Column(DateTime, nullable=False)
+    retweet_count = Column(Integer, default=0)
+    retweeted = Column(Boolean, default=False)
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+
+engine = create_engine('sqlite:///twittet_app.sqlite')
+# engine = create_engine('postgresql://scott:tiger@localhost/mydatabase')
+# engine = create_engine('postgresql://power_user:$poweruserpassword@ec2-52-32-217-129.us-west-2.compute.amazonaws.com:5432/tweet_db')
+
+
+Base.metadata.create_all(engine)
---
+++ download_data.py
@@ -0,0 +1,76 @@
+
+from datetime import datetime
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+import tweepy
+
+from twitter_credentials import *
+from declaration import Person, Status
+
+# connection to twitter
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+# connection to DB
+engine = create_engine('sqlite:///twittet_app.sqlite', echo=False)
+# engine = create_engine("""postgresql://
+# power_user:$poweruserpassword@
+# ec2-52-32-217-129.us-west-2.compute.amazonaws.com:5432/tweet_db""")
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+# set parameters
+user_name = 'analyticbridge'
+
+# saving user data
+person_full = api.get_user(user_name)
+j = person_full._json
+if session.query(Person).filter(Person.screen_name == user_name).count() == 0:
+	p =Person(
+				id = j['id'],
+				screen_name = j['screen_name'] ,
+				name = j['name'],
+				description = j['description'],
+				location = j['location'],
+				url = j['url'],
+				followers_count = j['followers_count'],
+				statuses_count = j['statuses_count'],
+				friends_count = j['friends_count'])
+	session.add(p)
+	session.commit()
+else:
+	p = session.query(Person).filter(Person.screen_name == user_name).one()
+	p.name = j['name']
+	p.description = j['description']
+	p.location = j['location']
+	p.url = j['url']
+	p.followers_count = j['followers_count']
+	p.statuses_count = j['statuses_count']
+	p.friends_count = j['friends_count']
+	session.commit()
+
+#saving tweets of user
+
+
+for status in tweepy.Cursor(api.user_timeline, id='analyticbridge', count = 200).items(200):
+	j = status._json
+	if session.query(Status).filter(Status.id == j['id_str']).count() == 0:
+		#add new tweet
+		created_at = datetime.strptime(j['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+		s = Status(
+			id = j['id_str'],
+			text = j['text'],
+			created_at = created_at,
+			retweet_count = j['retweet_count'],
+			retweeted = j['retweeted'],
+			person = p)
+		session.add(s)
+		#session.commit()
+		print("Tweet " + j['id_str'] + " is imported")
+	else:
+		#update the tweet
+		s = session.query(Status).filter(Status.id == j['id_str']).one()
+		retweet_count = j['retweet_count'],
+		#session.commit()
+session.commit()
